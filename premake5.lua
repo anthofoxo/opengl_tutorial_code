488db7d73b5f1d6edf56c1a84b4fb38134dec6a8
AnthoFoxo <anthofoxo@gmail.com>
2022-07-24 16:06:20 -0400

    1.4

--- premake5.lua
+++ premake5.lua
@@ -44,13 +44,9 @@ workspace "tutorial"
 	-- systemversion only applies to windows
 	filter "system:windows"
 		systemversion "latest"
--- Create the main project. Must match `startproject` in the workspace.
 project "tutorial"
-	-- Specify where the location of the project file should go.
 	location "tutorial"
-	-- Specify the project to create a console application.
 	kind "ConsoleApp"
-	-- Specify the files to include in the project.
 	files
 	{
 		"%{prj.location}/src/**.cpp",
@@ -58,5 +54,110 @@ project "tutorial"
 		"%{prj.location}/src/**.c",
 		"%{prj.location}/src/**.h"
 	}
-	-- Specify the include directories.
-	includedirs "%{prj.location}/src"
+	-- We must specify the glfw include directyory in addition to our existing one
+	includedirs
+	{
+		"%{prj.location}/src",
+		"%{wks.location}/vendor/glfw/include"
+	}
+	--[[
+	This section defines what to be linked with at compile time.
+	Premake makes it very easy to link other project just my specifying the project name
+	]]--
+	links "glfw"
+	--[[
+	On linux we must link with additional dependencies to allow glfw to function.
+	If these are not defined as a project, it will add the -l flag to the linker command.
+	]]--
+	filter "system:linux"
+		links
+		{
+			"dl",
+			"pthread",
+			"X11"
+		}
+	-- On macosx we must link against these
+	filter "system:macosx"
+		links
+		{
+			"CoreFoundation.framework",
+			"Cocoa.framework",
+			"IOKit.framework",
+			"CoreVideo.framework"
+		}
+project "glfw"
+	location "vendor/glfw"
+	-- We set the default language to C++, GLFW is a C api so we override it here
+	language "C"
+	-- These files must be compiled on all platforms
+	files
+	{
+		"%{prj.location}/src/context.c",
+		"%{prj.location}/src/egl_context.c",
+		"%{prj.location}/src/egl_context.h",
+		"%{prj.location}/src/init.c",
+		"%{prj.location}/src/input.c",
+		"%{prj.location}/src/internal.h",
+		"%{prj.location}/src/monitor.c",
+		"%{prj.location}/src/osmesa_context.c",
+		"%{prj.location}/src/osmesa_context.h",
+		"%{prj.location}/src/vulkan.c",
+		"%{prj.location}/src/window.c"
+	}
+	-- These files must be compiled on windows
+	filter "system:windows"
+		files
+		{
+			"%{prj.location}/src/wgl_context.c",
+			"%{prj.location}/src/wgl_context.h",
+			"%{prj.location}/src/win32_init.c",
+			"%{prj.location}/src/win32_joystick.c",
+			"%{prj.location}/src/win32_joystick.h",
+			"%{prj.location}/src/win32_monitor.c",
+			"%{prj.location}/src/win32_platform.h",
+			"%{prj.location}/src/win32_thread.c",
+			"%{prj.location}/src/win32_time.c",
+			"%{prj.location}/src/win32_window.c"
+		}
+		-- GLFW requires us to select the windowing API at compile time, for windows we use WIN32, 
+		-- additionally glfw doesn't use the windows CRT functions so we silence the warnings
+		defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
+	-- These files must be compiled on linux
+	filter "system:linux"
+		files
+		{
+			"%{prj.location}/src/glx_context.c",
+			"%{prj.location}/src/glx_context.h",
+			"%{prj.location}/src/linux_joystick.c",
+			"%{prj.location}/src/linux_joystick.h",
+			"%{prj.location}/src/posix_time.c",
+			"%{prj.location}/src/posix_time.h",
+			"%{prj.location}/src/posix_thread.c",
+			"%{prj.location}/src/posix_thread.h",
+			"%{prj.location}/src/x11_init.c",
+			"%{prj.location}/src/x11_monitor.c",
+			"%{prj.location}/src/x11_platform.h",
+			"%{prj.location}/src/x11_window.c",
+			"%{prj.location}/src/xkb_unicode.c",
+			"%{prj.location}/src/xkb_unicode.h"
+		}
+		-- For linux we are using the X11 api
+		defines "_GLFW_X11"
+	-- These files must be compiled on macosx
+	filter "system:macosx"
+		files
+		{
+			"%{prj.location}/src/cocoa_init.m",
+			"%{prj.location}/src/cocoa_joystick.m",
+			"%{prj.location}/src/cocoa_joystick.h",
+			"%{prj.location}/src/cocoa_monitor.m",
+			"%{prj.location}/src/cocoa_platform.h",
+			"%{prj.location}/src/cocoa_time.c",
+			"%{prj.location}/src/cocoa_window.m",
+			"%{prj.location}/src/nsgl_context.m",
+			"%{prj.location}/src/nsgl_context.h",
+			"%{prj.location}/src/posix_thread.c",
+			"%{prj.location}/src/posix_thread.h"
+		}
+		-- For macosx we are using the Cocoa api
+		defines "_GLFW_COCOA"
--- tutorial/src/engine/entry_point.cpp
+++ tutorial/src/engine/entry_point.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	// Initialize GLFW, this must be called before any other GLFW function
+	glfwInit();
+
+	// Create a window
+	// Note since C++11 we can use nullptr instead of NULL
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello Window", nullptr, nullptr);
+
+	// Run the main loop until the user closes the window
+	while (!glfwWindowShouldClose(window))
+	{
+		// Process events, such as keyboard input, resize, movement etc.
+		glfwPollEvents();
+	}
+
+	// After the window is closed, destroy the window and terminate GLFW
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
